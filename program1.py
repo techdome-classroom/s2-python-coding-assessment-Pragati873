efb6023b0381789f8ab1898884a12f2f9378dbef
Pragati873 <mishrapragati873@gmail.com>
2024-10-07 14:33:41 +0530

    Pragati Mishra

--- program1.py
+++ program1.py
@@ -5,7 +5,15 @@ class Solution(object):
         :rtype: bool
         """
         pass
-
+   
+        valid_brack = [('{', '}'), ('(', ')'), ('[', ']')]
+        stack = []
+        for c in s:
+            if len(stack)>0 and (stack[-1], c) in valid_brack:
+                stack.pop()
+            else:
+                stack.append(c)
+        return len(stack)==0
 
 
 
